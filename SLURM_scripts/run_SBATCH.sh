d053665038f837659568929c2602892831989e35
Gunjan Sethia (Gala) <ggg256@nyu.edu>
2017-06-07 19:08:13 -0400

    Update run_SBATCH.sh

--- SLURM_scripts/run_SBATCH.sh
+++ SLURM_scripts/run_SBATCH.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
 cd $(pwd)
 mkdir oe
 JOB_NAME="ALL"
 DATE=`date +%m-%d-%Y`
 
 sbatch --mail-type=ALL --mail-user=$USER@nyu.edu --job-name=$JOB_NAME -o $(pwd)/oe/%x_$DATE_%A.out -e $(pwd)/oe/%x_$DATE_%A.err /home/ggg256/scripts/5JUN_01to09.sh $(pwd)
-
-echo $DATE
-echo $JOB_NAME
-
