cde6fb79307327464da83c32df04efa8c3a7c408
Gunjan Sethia (Gala) <ggg256@nyu.edu>
2017-06-07 21:30:01 -0400

    + array id (%a) for e/o logs

--- SLURM_scripts/run_SBATCH.sh
+++ SLURM_scripts/run_SBATCH.sh
@@ -4,6 +4,6 @@ mkdir oe
 JOB_NAME="ALL"
 DATE=`date +%m-%d-%Y`
 
-sbatch --mail-type=ALL --mail-user=$USER@nyu.edu --job-name=$JOB_NAME -o $(pwd)/oe/%x_$DATE_%A.out -e $(pwd)/oe/%x_$DATE_%A.err /home/ggg256/scripts/5JUN_01to09.sh $(pwd)
+sbatch --mail-type=ALL --mail-user=$USER@nyu.edu --job-name=$JOB_NAME -o $(pwd)/oe/%x_$DATE_%A_%a.out -e $(pwd)/oe/%x_$DATE_%A_%a.err /home/ggg256/scripts/5JUN_01to09.sh $(pwd)
 
 echo "JOB SUBMITTED SUCCESSFULLY"
